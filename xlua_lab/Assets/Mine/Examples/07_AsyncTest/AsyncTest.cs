26e94bfe22cc318311ad45e20ab0bce26b4d3200
YAN JUNJIE <48432483+CodingCodingK@users.noreply.github.com>
2021-07-29 19:02:34 +0800

    0729

---
+++ xlua_lab/Assets/Mine/Examples/07_AsyncTest/AsyncTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+namespace Mine
+{
+    public class AsyncTest : MonoBehaviour
+    {
+        private LuaEnv luaenv = new LuaEnv();
+        // Start is called before the first frame update
+        void Start()
+        {
+            luaenv.DoString("require 'async_test'");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            luaenv?.Tick();
+        }
+    }
+
+}
